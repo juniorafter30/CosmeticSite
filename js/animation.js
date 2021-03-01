b35d6e0ab00c7317799086e3d3a1be668a765e45
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2021-03-01 17:18:44 +0100

    changes

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector('.hero');
 const backgroundSlider = document.querySelector('.background-slider');
-const logo = document.querySelector('#logo');
+const logo = document.querySelector('.logo');
 const backgroundCirle = document.querySelector('.background-circle-right');
 const navLinks = document.querySelector('.nav-links');
 const heroHeadline = document.querySelector('.hero-headline');
@@ -13,5 +13,6 @@ tl.fromTo(hero,1,{height: "0%"},{height:"80%", ease: Power2.easeInOut})
 .fromTo(heroHeadline,1,{opacity:'0%'},{opacity:"100%", ease: Power2.easeInOut})
 .fromTo(backgroundCirle,2,{y:'-100%'},{y:"0%", ease: Power2.easeInOut},"-=2")
 .fromTo(navLinks,1,{opacity:'0%'},{opacity:'100%', ease: Power2.easeInOut},"-=1.7")
+.fromTo(logo,1,{opacity:'0%'},{opacity:'100%', ease: Power2.easeInOut},"-=0.3")
 
 
